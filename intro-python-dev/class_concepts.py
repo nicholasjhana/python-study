ea539d1a0528641b94277f49acab6dfd3bac30e4
Nicholas <nicholas@nicholasjhana.com>
2020-04-02 15:44:29 +0200

    added example for inheretance

--- intro-python-dev/class_concepts.py
+++ intro-python-dev/class_concepts.py
@@ -35,6 +35,8 @@ class Tire:
     A tire represents part of a car. So can be consitered part of composition of the car.
 
     Composition allows us to separate concerns of one type from another. In this case, a car doesn't need to know how to calculate details for its tires. That would be the job of the Tire clas. The car class just uses this information.
+
+    Inheritance: A class that is based off of a different class. Same type of object but based off the parent.
     """
     def __init__(self, tire_type, width, ratio, diameter, brand="", construction='R'):
         self.tire_type = tire_type
@@ -53,7 +55,6 @@ class Tire:
     def circumference(self):
         """
         Circumference of the tire in inches.
-
         Uses a docstring test to check the implementation. Doctesting in this way prevents regresive errors.
 
         Doctests work by executing code inidcated by the >>> and checking versus the output (indicated with no indent) on the line below. We can run the doctest in the terminal with:
@@ -65,6 +66,58 @@ class Tire:
         80.1
         """
 
-        side_wall_inches = (self.width * (self.ratio / 100)) / 25.4
-        total_diameter = side_wall_inches * 2 + self.diameter
+        total_diameter = self._side_wall_inches() * 2 + self.diameter
         return round(total_diameter * math.pi, 1)
+
+    def _side_wall_inches(self):
+        """
+        We use this calculation as a hidden method. Known as a private method.
+
+        We create this because it allows us to use in inherited classes.
+        """
+        return (self.width * (self.ratio / 100)) / 25.4
+
+class SnowTire(Tire):
+    """
+    In this case we use inheritance ofthe Tire class because snow tire is the same 'thing' as a tire. The difference is it has some extra features/methods that dont' exist with stanard Tire class.
+    """
+
+    def __init__(self, tire_type, width, ratio, diameter, chain_thickness, brand='', construction='R'):
+        """
+        We do not need to write an __init__ because we will have all the details from the Tire __init__. Becuase we want to extend the number of variables we add an __init__.
+
+        One option is to define the init where we pass in variables in a typical way:
+        #def __init__(self, tire_type, width, ratio, diameter, chain_thickness, brand='', construction='R'):
+            # we pass in the values dicretly to the Tire __init__ function
+            #Tire.__init__(self, tire_type, width, ratio, diameter, branc, construction)
+            # Then we add the extra varibales like any other __init__
+            #self.chain_thickness = chain_thickness
+
+        In the example below we make use of the super() method that calls the ini from the parent class
+
+        """
+
+        super().__init__(self, tire_type, width, ratio, diameter, brand, construction)
+        self.chain_thickness = chain_thickness
+
+        def circumference(self):
+            """
+            The circumference function specific to the snow tire class.
+            We use a doctest like in the Tire class.
+
+            >>> tire = SnowTire('P', 205, 65, 15, 2)
+            >>> tire.circumference()
+            92.7
+            """
+            total_diameter = (self._side_wall_inches() + self.chain_thickness * 2 + self.diameter)
+            return round(total_diameter * math.pi, 1)
+
+        def __repr__(self):
+            """
+            We want this to return exactly like the __repr__ in the Tire class and add to it that we are using a sow tire. Todo this we use the super() method.
+
+            Super calls the method from the parent class. So in this case it is calling the __repr__ from the Tire class.
+            """
+            return super().__repr__() + "(Snow)"
+
+
